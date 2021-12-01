a0c10666038838afd1b81f3bb0a524cc43ee9152
Andrey Poymanov <poym4nov@yandex.ru>
2021-12-01 06:54:09 +0300

    edit return

--- script.js
+++ script.js
@@ -5,17 +5,16 @@ let str = prompt('Введите строку');
 const lexic = function (str) {
     if (typeof str === 'string') {
         if (!str) {
-            alert('Пустая строка');
-            return;
+            return 'Пустая строка';
         }
         str = str.trim();
         if (str.length > 30) {
             str = str.slice(0, 30) + '...';
         }
-        alert(str);
+        return str;
     } else {
-        alert('Это не строка, а ' + typeof str);
+        return 'Это не строка, а ' + typeof str;
     }
 };
 
-lexic(str);
+alert(lexic(str));

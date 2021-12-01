23caf99a24003ea047fbe1128c7e5d0ecd98a693
Andrey Poymanov <poym4nov@yandex.ru>
2021-12-01 06:43:27 +0300

    edit alert

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ let str = prompt('Введите строку');
 
 const lexic = function (str) {
     if (typeof str === 'string') {
+        if (!str) {
+            alert('Пустая строка');
+            return;
+        }
         str = str.trim();
         if (str.length > 30) {
             str = str.slice(0, 30) + '...';
         }
         alert(str);
     } else {
-        alert('Это не строка');
+        alert('Это не строка, а ' + typeof str);
     }
 };
 

a2c1ab8b6ba5088db7d71214656a307a8c67ca3e
Andrey Poymanov <poym4nov@yandex.ru>
2021-12-04 23:06:08 +0300

    fix

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 let week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
     outWeek = document.getElementById('week'),
-    day = new Date().getDay() - 1;
+    day = 0;//new Date().getDay() - 1;
 
 
 week.forEach(function (item, index) {
+    (day == 0) ? day = 6 : day;
     if (index == day) {
         outWeek.innerHTML += "<p><b>" + item + "</p></b>";
         return;
